f6a0cc4917570ebbb18317525515c8ecf18a4cb4
Adzerty <adzerty76@outlook.fr>
2020-11-08 00:09:18 +0100

    Ajout d'une ébauche de lobby

--- src/mafiaDeCuba/ihm/FrameLobbyHost.java
+++ src/mafiaDeCuba/ihm/FrameLobbyHost.java
@@ -1,10 +1,13 @@
 package mafiaDeCuba.ihm;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.List;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 
 import mafiaDeCuba.metier.Joueur;
@@ -35,10 +38,28 @@ public class FrameLobbyHost extends JFrame
 		this.setTitle("Lobby (hébergeur)");
 		this.setLocation( (largeurEcran/2)-350, (hauteurEcran/2)-250);
 		this.setSize(700,500);
+		this.setLayout(new BorderLayout());
 		
-		this.listConnectes = new JList()
+		this.listConnectes = new JList(alJ.toArray());
+		
+		this.add(this.listConnectes,BorderLayout.CENTER);
+		this.add(new JLabel("                      L O B B B Y"), BorderLayout.NORTH);
+		
+		System.out.println("Ca se lance ca");
 		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
+	
+	public void majIHM()
+	{
+		this.alJoueur = this.serveur.getAlJoueurCo();
+		this.listConnectes.setListData(this.alJoueur.toArray());
+	}
+	
+	public static void main(String[] args)
+	{
+		FrameLobbyHost fLobby = new FrameLobbyHost(null, null);
+	}
+
 }
